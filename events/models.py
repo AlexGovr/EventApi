54d8e5ff33430664fbbbd6838af32dda3a4260f3
Alex <sobachka.94@mail.ru>
2021-06-05 18:00:22 +0300

    add event model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from swingtime import models as stmodels
 
-# Create your models here.
+
+class Event(stmodels.Event):
+    city = models.CharField(max_length=60)
+    tickets = models.IntegerField()
+    cost = models.FloatField()
+    date_time = models.DateTimeField()
+    periodicity = models.CharField(max_length=10, choices = (
+        ('yearly', 'yearly'),
+        ('monthly', 'monthly'),
+        ('weekly', 'weekly'),
+        ('daily', 'daily'),
+    ))
