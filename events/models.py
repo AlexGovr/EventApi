76e242c281b7e85925e36613edbc956fb540ce22
Alex <sobachka.94@mail.ru>
2021-06-06 15:35:43 +0300

    implement Event query methods

--- events/models.py
+++ events/models.py
@@ -1,3 +1,6 @@
+
+import datetime
+from dateutil import rrule, relativedelta, tz
 from django.db import models
 
 
@@ -17,12 +20,53 @@ class Event(models.Model):
             ('daily', 'daily'),
             ('one-off', 'one-off'),
             ),
-        default='one-off')
+        default='one-off',
+        blank=False)
 
     @classmethod
     def get_month_occurrences(cls, month):
-        return cls.objects.all()
-    
+        print(month)
+        earliest = datetime.datetime.now(tz.UTC).replace(month=month, day=1)
+        print(earliest)
+        until = earliest + relativedelta.relativedelta(months=1)
+        objects = []
+        for o in cls.objects.filter(date__gte=earliest, tickets__gt=0):
+            if o.periodicity != 'one-off':
+                objects.extend(get_occurrences(o.date, until, o.periodicity, o))
+            else:
+                objects.append(o)
+        return objects
+
     @classmethod
     def get_upcoming_occurences(cls, city):
-        return cls.objects.filter(city=city)
+        earliest = datetime.datetime.now(tz.UTC)
+        until = earliest + relativedelta.relativedelta(months=1)
+        objects = []
+        for o in cls.objects.filter(date__gte=earliest, city=city, tickets__gt=0):
+            if o.periodicity != 'one-off':
+                objects.extend(get_occurrences(o.date, until, o.periodicity, o))
+            else:
+                objects.append(o)
+        return objects
+
+
+class EventClone:
+    '''simple Event object simulation
+    used to cast event occurences'''
+
+    fields = ['title', 'city', 'cost', 'tickets', ]
+
+    def __init__(self, event, **attrs):
+        for attr in self.fields:
+            val = getattr(event, attr)
+            setattr(self, attr, val)
+        for attr, val in attrs.items():
+            setattr(self, attr, val)
+
+
+def get_occurrences(date, until, freq, event):
+    # retrive rrule corresponding freq value
+    rrule_freq = getattr(rrule, freq.upper())
+    # cast datetimes via rrule
+    occur = list(rrule.rrule(rrule_freq, dtstart=date, until=until))
+    return [EventClone(event, date=dt) for dt in occur]
