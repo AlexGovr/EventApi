f0a36af48a792a092fd445891e3e8724df703bce
Alexander Govorov <alex_govr@mail.ru>
2021-06-08 09:04:03 +0300

    exclude field

--- events/serializers.py
+++ events/serializers.py
@@ -19,5 +19,5 @@ class EventQuerySerializer(serializers.Serializer):
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
-        fields = '__all__'
         read_only = '__all__'
+        exclude = ['cost']
