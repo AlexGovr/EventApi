ebdfc029db73878c4114ccbaf52a240c20c6c84e
Alexander Govorov <alex_govr@mail.ru>
2021-06-08 09:04:24 +0300

    add tests

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,29 @@
+from datetime import datetime
+from dateutil import tz, relativedelta
 from django.test import TestCase
+from events.models import get_occurrences, Event, EventClone
+from test_api import shift_from_now
 
-# Create your tests here.
+
+class Test(TestCase):
+
+    def setUp(self):
+        Event.objects.create(**{
+            'title': 'Title4',
+            'city': 'City3',
+            'cost': 500,
+            'tickets': 1,
+            'date': shift_from_now(-10),
+            'periodicity': 'weekly',
+        })
+
+    def test_get_occurences(self):
+        e = Event.objects.get(id=1)
+        until = datetime.now(tz.UTC) + relativedelta.relativedelta(months=1)
+        expected = [
+            EventClone(e, date=shift_from_now(4)),
+            EventClone(e, date=shift_from_now(11)),
+            EventClone(e, date=shift_from_now(18)),
+            EventClone(e, date=shift_from_now(25)),
+        ]
+        self.assertEqual(get_occurrences(e, until), expected)
--- test_api.py
+++ test_api.py
@@ -1,7 +1,8 @@
 
-from datetime import datetime, time, timedelta
+import calendar
+from datetime import datetime, timedelta
+from dateutil import tz
 from rest_framework.test import APITestCase
-from rest_framework.authtoken.models import Token
 from rest_framework import status
 from events.models import User, Event
 
@@ -33,50 +34,98 @@ class EventTests(APITestCase):
             self.assertEqual(response.status_code, status.HTTP_201_CREATED, msg=response)
 
     def test_get_upcoming(self):
+        # prepare data
         days_delta = -5
-        print(datetimestr(days_delta))
-        ev_data = {'title': 'Title4', 'city': 'City3', 'cost': 500,
-             'tickets': 100, 'date': datetimestr(days_delta), 'periodicity': 'weekly'}
-        e = Event(**ev_data)
-        e.save()
+        ev_data = {
+            'title': 'Title4',
+            'city': 'City3',
+            'cost': 500,
+            'tickets': 100,
+            'date': datetimestr(days_delta),
+            'periodicity': 'weekly'
+        }
+        Event.objects.create(**ev_data)
         expected = [
             {**ev_data, 'date': datetimestr(days_delta + 7)},
             {**ev_data, 'date': datetimestr(days_delta + 14)},
             {**ev_data, 'date': datetimestr(days_delta + 21)},
             {**ev_data, 'date': datetimestr(days_delta + 28)},
+            {**ev_data, 'date': datetimestr(days_delta + 35)},
         ]
         _ = [e.pop('periodicity') for e in expected]
         response = self.client.get('/get-events/upcoming?city=City3')
         self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.json())
         msg=f'{response.json()}\n{expected}'
-        print(*response.json(), sep='\n')
-        print(*expected, sep='\n')
         self.assertEqual(response.json(), expected, msg=msg)
 
     def test_get_month(self):
+        # prepare data
         days_delta = -5
-        print(datetimestr(days_delta))
-        ev_data = {'title': 'Title5', 'city': 'City3', 'cost': 500,
-             'tickets': 100, 'date': datetimestr(days_delta), 'periodicity': 'weekly'}
-        e = Event(**ev_data)
-        e.save()
-        expected = [
+        ev_data = {
+            'title': 'Title4',
+            'city': 'City3',
+            'cost': 500,
+            'tickets': 100,
+            'date': datetimestr(days_delta),
+            'periodicity': 'weekly'
+        }
+        ev1_data = {
+            'title': 'Title5',
+            'city': 'City3',
+            'cost': 500,
+            'tickets': 100,
+            'date': datetimestr(10),
+            'periodicity': 'one-off'
+        }
+        Event.objects.create(**ev_data)
+        Event.objects.create(**ev1_data)
+        cur_month = calendar.month_abbr[datetime.now().month]
+        expected = sorted([
             {**ev_data, 'date': datetimestr(days_delta + 7)},
             {**ev_data, 'date': datetimestr(days_delta + 14)},
             {**ev_data, 'date': datetimestr(days_delta + 21)},
-            {**ev_data, 'date': datetimestr(days_delta + 28)},
-        ]
+            ev1_data,
+        ], key=lambda d: d['date'])
         _ = [e.pop('periodicity') for e in expected]
-        response = self.client.get('/get-events/month?month=june')
-        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.data)
-        self.assertEqual(response.data, {})
 
-    # def test_payment(self):
-    #     response = self.client.post('/buy-ticket', auth=('alex','1'), data={'user_id': 1, 'event': 1})
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED, msg=response.data)
-    #     self.assertEqual(response.data, {})
+        response = self.client.get(f'/get-events/month?month={cur_month}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.data)
+        data = response.data
+        data.sort(key=lambda d: d['date'])
+        self.assertEqual(data, expected)
+
+    def test_payment(self):
+        # prepare data
+        self.client.login(username='Steven', password='Jobs')
+        ev_data = {
+            'title': 'Title4',
+            'city': 'City3',
+            'cost': 500,
+            'tickets': 1,
+            'date': datetimestr(3),
+            'periodicity': 'weekly',
+            'id': 10
+        }
+        Event.objects.create(**ev_data)
+
+        # check payment
+        response = self.client.post('/buy-ticket', data={'user_id': 1, 'event': 10, 'transaction_id': 1})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, msg=response.data)
+        # check tickets number changed
+        expected = ev_data
+        expected['tickets'] -= 1
+        response = self.client.get('/event/10')
+        self.assertEqual(response.json(), ev_data)
+        # get no-tickets response
+        response = self.client.post('/buy-ticket', data={'user_id': 1, 'event': 10, 'transaction_id': 1})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, msg=response.data)
 
 
 def datetimestr(days_delta=0):
-    date = datetime.today() + timedelta(days=days_delta)
+    date = shift_from_now(days_delta)
     return datetime.strftime(date, '%Y-%m-%dT%H:%M:%SZ')
+
+
+def shift_from_now(days):
+    res = datetime.now(tz.UTC) + timedelta(days=days)
+    return res.replace(microsecond=0)
